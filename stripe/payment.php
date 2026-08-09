3b27bcde36695347f42f3b6ca32c060bb31f82c4
Kevin Hilton <kevin.hilton@prodigyworks.co.uk>
2026-08-09 05:22:23 +0100

    stripe

--- stripe/payment.php
+++ stripe/payment.php
@@ -3,7 +3,6 @@
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Headers: Content-Type");
     header("Access-Control-Allow-Methods: POST");
-    header("Content-Type: application/json");
 
     // Load Stripe
     require __DIR__ . '/vendor/autoload.php';
